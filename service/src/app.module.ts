cb53fe79e880d92fb22c4bc0931fbcb15438ea6a
Michael Gambold <7439294+michaelgambold@users.noreply.github.com>
2022-06-01 23:55:21 +1000

    ➕ Add required dependencies

--- service/src/app.module.ts
+++ service/src/app.module.ts
@@ -10,6 +10,9 @@ import { FuelSeeder } from './seeders/FuelSeeder';
 import { RegionSeeder } from './seeders/RegionSeeder';
 import { DataScraperModule } from './data-scraper/data-scraper.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import { DataModule } from './data/data.module';
+import { FuelModule } from './fuel/fuel.module';
+import { RegionModule } from './region/region.module';
 
 @Module({
   imports: [
@@ -20,6 +23,9 @@ import { ScheduleModule } from '@nestjs/schedule';
     MikroOrmModule.forRoot(),
     ScheduleModule.forRoot(),
     DataScraperModule,
+    DataModule,
+    FuelModule,
+    RegionModule,
   ],
   controllers: [AppController],
   providers: [AppService],
