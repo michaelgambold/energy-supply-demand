66384165ca6391f2efc68a4b7bab5dc42752c2b2
Michael Gambold <7439294+michaelgambold@users.noreply.github.com>
2022-06-10 20:52:07 +1000

    ⚰️ Remove socket module

--- service/src/app.module.ts
+++ service/src/app.module.ts
@@ -1,17 +1,18 @@
 import { MikroORM } from '@mikro-orm/core';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+
 import { FuelSeeder } from './seeders/FuelSeeder';
 import { RegionSeeder } from './seeders/RegionSeeder';
 import { DataScraperModule } from './data-scraper/data-scraper.module';
-import { ScheduleModule } from '@nestjs/schedule';
 import { DataModule } from './data/data.module';
 import { FuelModule } from './fuel/fuel.module';
 import { RegionModule } from './region/region.module';
-import { SocketModule } from './socket/socket.module';
 import { PowerModule } from './power/power.module';
 import { PowerSeeder } from './seeders/PowerSeeder';
 
@@ -21,13 +22,27 @@ import { PowerSeeder } from './seeders/PowerSeeder';
       rootPath: join(__dirname, '..', '..', 'web', 'dist'),
     }),
     ConfigModule.forRoot(),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          redis: {
+            host: config.get('REDIS_HOST'),
+            port: config.get('REDIS_PORT'),
+          },
+        };
+      },
+    }),
+    BullModule.registerQueue({
+      name: 'new-data',
+    }),
     MikroOrmModule.forRoot(),
     ScheduleModule.forRoot(),
     DataScraperModule,
     DataModule,
     FuelModule,
     RegionModule,
-    SocketModule,
     PowerModule,
   ],
   controllers: [],
