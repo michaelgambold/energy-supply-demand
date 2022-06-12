d5ed5a91725952718cfa25ed21c74dcbba6456c0
Michael Gambold <7439294+michaelgambold@users.noreply.github.com>
2022-06-12 21:14:29 +1000

    ✨ Handle getting latest data when connecting to socket

--- service/src/data/data.gateway.ts
+++ service/src/data/data.gateway.ts
@@ -1,15 +1,17 @@
+import { MikroORM, UseRequestContext } from '@mikro-orm/core';
 import { Processor, Process } from '@nestjs/bull';
 import { Logger } from '@nestjs/common';
 import {
+  ConnectedSocket,
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
 import { Job } from 'bull';
-// import { SocketService } from './socket.service';
-// import { CreateSocketDto } from './dto/create-socket.dto';
-// import { UpdateSocketDto } from './dto/update-socket.dto';
 import { Socket } from 'socket.io';
+import { DataFact } from '../entities/DataFact.entity';
+import { DataService } from './data.service';
 import { DataDto } from './dto/data.dto';
 
 @WebSocketGateway({ namespace: 'data' })
@@ -18,15 +20,7 @@ export class DataGateway implements OnGatewayConnection, OnGatewayDisconnect {
   private readonly logger = new Logger(DataGateway.name);
   private readonly wsClients: Socket[] = [];
 
-  // constructor(private readonly socketService: SocketService) {
-  constructor() {
-    // setInterval(() => {
-    //   console.log('emitting hello to ' + this.wsClients.length + ' clients');
-    //   this.wsClients.forEach((c) => {
-    //     c.emit('hello', '🐈');
-    //   });
-    // }, 10000);
-  }
+  constructor(private readonly orm: MikroORM) {}
 
   handleConnection(client: Socket, ...args: any[]) {
     this.wsClients.push(client);
@@ -46,6 +40,7 @@ export class DataGateway implements OnGatewayConnection, OnGatewayDisconnect {
     this.logger.log(`There are now ${this.wsClients.length} clients connected`);
   }
 
+  // process bull queue event to send new data to clients
   @Process()
   handleNewData(job: Job<DataDto>) {
     this.logger.log('Sending new data to websocket clients');
@@ -54,12 +49,58 @@ export class DataGateway implements OnGatewayConnection, OnGatewayDisconnect {
     }
   }
 
-  // KEEP: working example of pushing to socket from client and responding
-  // @SubscribeMessage('liveData')
-  // liveData(@ConnectedSocket() client: any) {
-  //   // console.log(client);
-  //   console.log('live data subcription 🐈');
-  //   client.send('liveData2', 'hi back');
-  //   return 'hi';
-  // }
+  @SubscribeMessage('get-latest-data')
+  @UseRequestContext()
+  async liveData(@ConnectedSocket() client: Socket): Promise<DataDto> {
+    this.logger.log('Getting/sending latest data to websocket clients');
+
+    const dataFactRepository = this.orm.em.getRepository(DataFact);
+
+    // get latest timestamp
+    const maxTimestamp = await dataFactRepository.findAll({
+      orderBy: { timestamp: 'desc' },
+      limit: 1,
+    });
+
+    const dataFacts = await dataFactRepository.find(
+      {
+        timestamp: { $eq: maxTimestamp[0].timestamp },
+      },
+      {
+        populate: ['fuel', 'region', 'power'],
+      },
+    );
+
+    return dataFacts.reduce(
+      (dto: DataDto, dataFact) => {
+        if (!dto.metadata.fuels.includes(dataFact.fuel)) {
+          dto.metadata.fuels.push(dataFact.fuel);
+        }
+        if (!dto.metadata.power.includes(dataFact.power)) {
+          dto.metadata.power.push(dataFact.power);
+        }
+        if (!dto.metadata.regions.includes(dataFact.region)) {
+          dto.metadata.regions.push(dataFact.region);
+        }
+
+        dto.data.push({
+          fuelId: dataFact.fuel.id,
+          powerId: dataFact.power.id,
+          regionId: dataFact.region.id,
+          timestamp: dataFact.timestamp,
+          value: dataFact.value,
+        });
+
+        return dto;
+      },
+      {
+        metadata: {
+          fuels: [],
+          power: [],
+          regions: [],
+        },
+        data: [],
+      },
+    );
+  }
 }
--- web/src/app/pages/live-data-page/live-data-page.component.ts
+++ web/src/app/pages/live-data-page/live-data-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { Subject, Subscription } from 'rxjs';
 import { io, Socket } from 'socket.io-client';
 import { Data } from '../../shared/models/data';
 import { DataService } from '../../shared/services/data.service';
@@ -9,30 +9,48 @@ import { DataService } from '../../shared/services/data.service';
   templateUrl: './live-data-page.component.html',
   styleUrls: ['./live-data-page.component.css'],
 })
-export class LiveDataPageComponent implements OnInit, OnDestroy {
+export class LiveDataPageComponent implements OnInit, OnDestroy, OnDestroy {
   data$ = new Subject<Data>();
+  lastUpdatedAt?: Date;
 
   private socket!: Socket;
+  private dataSub!: Subscription;
 
   constructor(private readonly dataService: DataService) {}
 
   ngOnInit(): void {
+    this.dataSub = this.data$.subscribe(() => {
+      this.lastUpdatedAt = new Date();
+    });
+
     this.socket = io('/data');
 
     this.socket.on('connect', () => {
-      console.log('connected to socket');
+      console.log('connected to /data socket');
     });
 
     this.socket.on('error', (error) => {
       console.warn(error);
     });
 
+    this.socket.on('disconnect', () => {
+      console.log('disconnected from /data socket');
+    });
+
+    // subscribe to new data event from backend
     this.socket.on('new-data', (data: Data) => {
+      this.data$.next(data);
+    });
+
+    // get latest data immediately instead of waiting for the next new data tick
+    this.socket.emit('get-latest-data', (data: Data) => {
+      console.log(JSON.stringify(data));
       this.data$.next(data);
     });
   }
 
   ngOnDestroy(): void {
     this.socket.disconnect();
+    this.dataSub.unsubscribe();
   }
 }
