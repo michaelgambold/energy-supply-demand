7700b840a926518d8dcf8bb280170fa6bc397885
Michael Gambold <7439294+michaelgambold@users.noreply.github.com>
2022-06-18 21:36:27 +1000

    🚧 Created 5 min data aggreation

--- service/src/data/data.controller.ts
+++ service/src/data/data.controller.ts
@@ -11,6 +11,9 @@ import { DataFact } from '../entities/DataFact.entity';
 import { FuelDimension } from '../entities/FuelDimension.entity';
 import { PowerDimension } from '../entities/PowerDimension.entity';
 import { RegionDimension } from '../entities/RegionDimension.entity';
+import { FuelService } from '../fuel/fuel.service';
+import { PowerService } from '../power/power.service';
+import { RegionService } from '../region/region.service';
 import { DataService } from './data.service';
 import { DataDto } from './dto/data.dto';
 
@@ -36,7 +39,12 @@ type Period =
 export class DataController {
   private readonly logger = new Logger(DataController.name);
 
-  constructor(private readonly dataService: DataService) {}
+  constructor(
+    private readonly dataService: DataService,
+    private readonly fuelService: FuelService,
+    private readonly powerService: PowerService,
+    private readonly regionService: RegionService,
+  ) {}
 
   @Get('/timerange/:timerange/period/:period')
   async findData(
@@ -45,9 +53,15 @@ export class DataController {
     @Query('fuel') fuel: string,
     @Query('region') region: string,
     @Query('power') power: string,
-  ): Promise<DataDto> {
+  ) {
     if (!timeRange) throw new BadRequestException('Time range cannot be null');
     if (!period) throw new BadRequestException('Period cannot be null');
+
+    const [fuels, regions, powers] = await Promise.all([
+      this.fuelService.findAll(),
+      this.regionService.findAll(),
+      this.powerService.findAll(),
+    ]);
 
     const regionId = region && parseInt(region);
     const powerId = power && parseInt(power);
@@ -81,58 +95,134 @@ export class DataController {
       case '2Weeks':
         startDate = subWeeks(endDate, 2);
         break;
-
       default:
         throw new BadRequestException('Invalid time range');
     }
 
-    const data = await this.dataService.findDataRange({
-      startDate,
-      endDate,
-      regionId,
-      fuelId,
-      powerId,
-    });
+    const mapToDto = (
+      input: {
+        fuelId: number;
+        regionId: number;
+        powerId: number;
+        timestamp: Date;
+        value: number;
+      }[],
+    ): DataDto => {
+      const dataDto: DataDto = input.reduce(
+        (dto: DataDto, input) => {
+          const f = fuels.find((x) => x.id === input.fuelId);
+          const r = regions.find((x) => x.id === input.regionId);
+          const p = powers.find((x) => x.id === powerId);
+
+          // add meta data if it does not exist already
+          if (!dto.metadata.fuels.includes(f)) {
+            dto.metadata.fuels.push(f);
+          }
+
+          if (!dto.metadata.power.includes(p)) {
+            dto.metadata.power.push(p);
+          }
+
+          if (!dto.metadata.regions.includes(r)) {
+            dto.metadata.regions.push(r);
+          }
+
+          // add data point
+          dto.data.push({
+            fuelId: input.fuelId,
+            powerId: input.powerId,
+            regionId: input.regionId,
+            timestamp: input.timestamp,
+            value: input.value,
+          });
+
+          return dto;
+        },
+        {
+          metadata: {
+            fuels: [],
+            regions: [],
+            power: [],
+          },
+          data: [],
+        },
+      );
+
+      return dataDto;
+    };
+
+    switch (period) {
+      case '1Minute':
+        const oneMinData = (
+          await this.dataService.findDataRange1MinutePeriod({
+            startDate,
+            endDate,
+            regionId,
+            fuelId,
+            powerId,
+          })
+        ).map((x) => {
+          return {
+            fuelId: x.fuel,
+            regionId: x.region,
+            powerId: x.power,
+            timestamp: x.timestamp,
+            value: x.value,
+          };
+        });
+        return mapToDto(oneMinData);
+      case '5Minutes':
+        const fiveMinData = await this.dataService.findDataRange5MinutePeriod({
+          startDate,
+          endDate,
+          regionId,
+          fuelId,
+          powerId,
+        });
+        return mapToDto(fiveMinData);
+      default:
+        throw new BadRequestException('Invalid period');
+    }
 
     // TODO: re-aggregate the data for different periods
 
-    const dataDto: DataDto = data.reduce(
-      (dto: DataDto, df: DataFact) => {
-        // add meta data if it does not exist already
-        if (!dto.metadata.fuels.includes(df.fuel)) {
-          dto.metadata.fuels.push(df.fuel);
-        }
+    // const dataDto: DataDto = data.reduce(
+    //   (dto: DataDto, df: DataFact) => {
+    //     // add meta data if it does not exist already
+    //     if (!dto.metadata.fuels.includes(df.fuel)) {
+    //       dto.metadata.fuels.push(df.fuel);
+    //     }
 
-        if (!dto.metadata.power.includes(df.power)) {
-          dto.metadata.power.push(df.power);
-        }
+    //     if (!dto.metadata.power.includes(df.power)) {
+    //       dto.metadata.power.push(df.power);
+    //     }
 
-        if (!dto.metadata.regions.includes(df.region)) {
-          dto.metadata.regions.push(df.region);
-        }
+    //     if (!dto.metadata.regions.includes(df.region)) {
+    //       dto.metadata.regions.push(df.region);
+    //     }
 
-        // add data point
-        dto.data.push({
-          fuelId: df.fuel.id,
-          powerId: df.power.id,
-          regionId: df.region.id,
-          timestamp: df.timestamp,
-          value: df.value,
-        });
+    //     // add data point
+    //     dto.data.push({
+    //       fuelId: df.fuel.id,
+    //       powerId: df.power.id,
+    //       regionId: df.region.id,
+    //       timestamp: df.timestamp,
+    //       value: df.value,
+    //     });
 
-        return dto;
-      },
-      {
-        metadata: {
-          fuels: [],
-          regions: [],
-          power: [],
-        },
-        data: [],
-      },
-    );
+    //     return dto;
+    //   },
+    //   {
+    //     metadata: {
+    //       fuels: [],
+    //       regions: [],
+    //       power: [],
+    //     },
+    //     data: [],
+    //   },
+    // );
 
-    return dataDto;
+    // return dataDto;
 
     // return {
     //   metadata: {
--- service/src/data/data.service.ts
+++ service/src/data/data.service.ts
@@ -1,3 +1,4 @@
+import { QueryOrder } from '@mikro-orm/core';
 import { InjectRepository } from '@mikro-orm/nestjs';
 import { EntityRepository } from '@mikro-orm/postgresql';
 import { Injectable, Logger } from '@nestjs/common';
@@ -12,18 +13,34 @@ export class DataService {
     private dataFactRepository: EntityRepository<DataFact>,
   ) {}
 
-  async findDataRange(args: {
+  /**
+   * Find data range with 1 minute data period
+   * @param args
+   * @returns
+   */
+  async findDataRange1MinutePeriod(args: {
     startDate: Date;
     endDate: Date;
     fuelId?: number;
     powerId?: number;
     regionId?: number;
-  }) {
-    this.logger.log('Find data range');
+  }): Promise<
+    {
+      uuid: string;
+      timestamp: Date;
+      value: number;
+      fuel: number;
+      region: number;
+      power: number;
+      date: number;
+      time: number;
+    }[]
+  > {
+    this.logger.log('Find data range with 1 minute period');
 
     const qb = this.dataFactRepository
       .qb()
-      .select('uuid')
+      .select('*')
       .where({ timestamp: { $gte: args.startDate } })
       .andWhere({ timestamp: { $lte: args.endDate } });
 
@@ -39,16 +56,67 @@ export class DataService {
       qb.andWhere({ region: args.regionId });
     }
 
-    const uuids = (await qb.execute()).map((x) => x.uuid);
+    qb.orderBy({ timestamp: QueryOrder.ASC });
 
-    return this.dataFactRepository.find(
-      {
-        uuid: { $in: uuids },
-      },
-      {
-        populate: ['fuel', 'region', 'power'],
-        orderBy: { timestamp: 'asc' },
-      },
-    );
+    return qb.execute();
+  }
+
+  async findDataRange5MinutePeriod(args: {
+    startDate: Date;
+    endDate: Date;
+    fuelId?: number;
+    powerId?: number;
+    regionId?: number;
+  }): Promise<
+    {
+      year: number;
+      monthNumber: number;
+      dayOfMonth: number;
+      hour: number;
+      twelfthOfHour: number;
+      fuelId: number;
+      regionId: number;
+      powerId: number;
+      timestamp: Date;
+      value: number;
+    }[]
+  > {
+    this.logger.log('Find data range with 1 minute period');
+
+    const qb = this.dataFactRepository
+      .qb('df')
+      .select([
+        'dd.year as year',
+        'dd.month_number as monthNumber',
+        'dd.day_of_month as dayOfMonth',
+        'td.hour as hour',
+        'td.twelfth_of_hour as twelfthOfHour',
+        'df.fuel_id as fuelId',
+        'df.region_id as regionId',
+        'df.power_id as powerId',
+        'min(timestamp) as timestamp',
+        'avg(value) as value',
+      ])
+      .join('df.date', 'dd')
+      .join('df.time', 'td')
+      .groupBy([
+        'dd.year',
+        'dd.month_number',
+        'dd.day_of_month',
+        'td.hour',
+        'td.twelfth_of_hour',
+        'df.fuel_id',
+        'df.region_id',
+        'df.power_id',
+      ])
+      .orderBy([
+        { date: { year: QueryOrder.ASC } },
+        { date: { monthNumber: QueryOrder.ASC } },
+        { date: { dayOfMonth: QueryOrder.ASC } },
+        { time: { hour: QueryOrder.ASC } },
+        { time: { twelfthOfHour: QueryOrder.ASC } },
+      ]);
+
+    return qb.execute();
   }
 }
